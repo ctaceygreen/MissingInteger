aa83afa4e848be0d8f384e0af9b8b03416d50a8b
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:53:44 +0000

    Add project files.

---
+++ MissingInteger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        HashSet<int> numbersInA = new HashSet<int>();
+        foreach(var item in A)
+        {
+            numbersInA.Add(item);
+        }
+
+        int number = 1;
+        bool found = false;
+        while(!found)
+        {
+            if(!numbersInA.Contains(number))
+            {
+                return number;
+            }
+            number++;
+        }
+        return 0;
+    }
+}
